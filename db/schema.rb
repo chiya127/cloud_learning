fb99ec8bf4fd5f7aee3db13a27aec73531103488
Claymore06 <itou.taichi@gmail.com>
2016-03-26 23:39:29 +0900

    fix migrationfile(answer table) syntax errror

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114183726) do
+ActiveRecord::Schema.define(version: 20160212125138) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id", limit: 4,   null: false
+    t.string   "body",        limit: 255, null: false
+    t.string   "note",        limit: 255, null: false
+    t.string   "profile",     limit: 255, null: false
+    t.string   "result",      limit: 255, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "subject",    limit: 255, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "category_users", force: :cascade do |t|
+    t.string   "display",     limit: 255, null: false
+    t.integer  "user_id",     limit: 4,   null: false
+    t.integer  "category_id", limit: 4,   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "group_users", force: :cascade do |t|
     t.integer  "group_id",   limit: 4, null: false
     t.integer  "user_id",    limit: 4, null: false
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "groups", force: :cascade do |t|
     t.integer  "tenant_id",  limit: 4,   null: false
     t.string   "name",       limit: 255, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -32,18 +56,33 @@ ActiveRecord::Schema.define(version: 20151114183726) do
     t.string   "body",        limit: 255, null: false
     t.string   "profile",     limit: 255, null: false
     t.string   "form",        limit: 255, null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.integer  "user_id",     limit: 4, null: false
+    t.integer  "category_id", limit: 4, null: false
+    t.integer  "question",    limit: 4, null: false
+    t.integer  "score",       limit: 4, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tenants", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "account",    limit: 255,                 null: false
-    t.string   "password",   limit: 255,                 null: false
+    t.string   "acount",     limit: 255,                 null: false
+    t.string   "password",   limit: 255
     t.boolean  "admin",      limit: 1,   default: false, null: false
-    t.string   "creator",    limit: 255,                 null: false
+    t.boolean  "creator",    limit: 1
     t.string   "email",      limit: 255,                 null: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
