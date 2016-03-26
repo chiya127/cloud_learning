6388aa36e7783c6789440fce794f011451a719b7
Claymore06 <itou.taichi@gmail.com>
2016-03-26 23:37:32 +0900

    fix syntax error

--- db/migrate/20160122121920_create_answers.rb
+++ db/migrate/20160122121920_create_answers.rb
@@ -1,11 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.intrger question_id, null: false
+      t.integer :question_id, null: false
       t.string :body, null: false
-      t.string :note, null false
+      t.string :note, null: false
       t.string :profile, null: false
-      t.string :result, null false
+      t.string :result, null: false
       t.timestamps
     end
   end
