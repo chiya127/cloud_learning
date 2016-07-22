b4474b472414144e6448cd52ae561d29148e8ca6
s-inagawa <s-inagawa@r-learning.co.jp>
2016-07-23 07:37:27 +0900

    今まで別のリポジトリで管理していたものをアールのリポジトリに移動

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,2 +1,37 @@
 class TenantsController < ApplicationController
+
+  before_action :find_tenant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tenants = Tenant.all
+  end
+
+  def new
+    @tenant = Tenant.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @tenant.destroy
+  end
+
+  private
+
+  def find_tenant
+    @tenant = Tenant.find(params[:id])
+  end
+
+  def tenant_params
+  end
 end
